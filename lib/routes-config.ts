42dadb01eed384876752222ff37a56157d5585aa
TristanHourtoulle <tristan.hourtoulle@epitech.eu>
2024-11-11 15:14:26 +0800

    add all page to the sidebar

--- lib/routes-config.ts
+++ lib/routes-config.ts
@@ -53,11 +53,50 @@ export const ROUTES: EachRoute[] = [
     items: [
       { title: "Introduction", href: "/introduction" },
       { title: "Installation", href: "/installation" },
-      { title: "Adding Features", href: "/adding-features" },
+      // { title: "Adding Features", href: "/adding-features" },
+      // {
+      //   title: "Understanding The Architecture",
+      //   href: "/understanding-the-architecture",
+      // },
+    ],
+  },
+  {
+    title: "Development Guide",
+    href: "/development-guide",
+    noLink: true,
+    items: [
+      { title: "Adding Page", href: "/adding-page" },
       {
-        title: "Understanding The Architecture",
-        href: "/understanding-the-architecture",
+        title: "Creating Customizable Components",
+        href: "/creating-customizable-components",
       },
+      {
+        title: "Styling with ShadcnUI & Tailwind",
+        href: "/styling-with-shadcnui-and-tailwind",
+      },
+      { title: "Setting up API Routes", href: "/setting-up-api-routes" },
+      { title: "Environment Variables", href: "/environment-variables" },
+      {
+        title: "Form Handling & Validation",
+        href: "/form-handling-and-validation",
+      },
+      {
+        title: "Custom Hooks & Utility Functions",
+        href: "/custom-hooks-and-utility-functions",
+      },
+      { title: "Testing & Debugging", href: "/testing-and-debugging" },
+      { title: "Optimazing Performance", href: "/optimizing-performance" },
+      {
+        title: "Error Handling & Logging",
+        href: "/error-handling-and-logging",
+      },
+      { title: "SEO & Metadata", href: "/configuring-seo-and-metadata" },
+      {
+        title: "Git & Version Control",
+        href: "/working-with-git-and-version-control",
+      },
+      { title: "CI/CD Vercel", href: "/cicd-with-vercel" },
+      { title: "Deploying", href: "/deploying-to-production" },
     ],
   },
 ];
